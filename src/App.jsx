70dca57b50715618a62cd25e146977d35d4f02bf
premkumar199131 <premkumar.ashokan@talentship.io>
2025-02-11 09:28:22 +0530

    new project code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import SideBar from "./components/SideBar"
+
+
+const App = () => {
+  return (
+ <>
+ <main className="h-screen my-5">
+<SideBar/>
+ </main>
+ </>
+  )
+}
+
+export default App
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,16 @@
+
+
+const SideBar = () => {
+  return (
+    <aside className="bg-black w-1/3 py-16 px-8 text-white md:w-72 rounded-r-xl h-screen">
+    <h2 className="mb-8 text-2xl font-bold uppercase ">Your Project</h2>
+    <div>
+        <button className="px-4 py-2 text-xs md:text-xl bg-stone-700 text-stone-400 rounded-md hover:text-stone-100 hover:bg-stone-500"> + Add Project</button>
+    </div>
+    <ul></ul>
+</aside>
+
+  )
+}
+
+export default SideBar
