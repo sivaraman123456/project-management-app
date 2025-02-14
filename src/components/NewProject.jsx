24be3d2c90f85781a488e2abf3dc2a35c82deda7
premkumar199131 <premkumar.ashokan@talentship.io>
2025-02-14 09:12:04 +0530

    Add project feature

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,33 +1,46 @@
 import { useRef } from "react";
-import Input from './Input'
-// eslint-disable-next-line react/prop-types
-const NewProject = ({onCancel}) => {
-const title = useRef()
-const description = useRef()
-const dueDate = useRef()
+import Input from "./Input";
 
-const handleSave = () => {
-  console.log('handleSave');
-  
-  const title2 = title.current.value;
-  const des = description.current.value;
-  const dueDate2 = dueDate.current.value;
+const NewProject = ({ onCancel, onAdd }) => {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
 
-  console.log({title2,des,dueDate2});
-  
-}
+  const handleSave = () => {
+    console.log("handleSave");
+
+    const title2 = title.current?.value;
+    const des = description.current?.value;
+    const dueDate2 = dueDate.current?.value;
+
+    console.log({ title2, des, dueDate2 });
+    // if(title.trim() === '' || des.trim() === '' || dueDate2.trim() === ''){
+
+    // }
+    onAdd({ title: title2, description: des, dueDate: dueDate2 });
+  };
 
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex justify-end gap-8 items-center">
         <li>
-          <button className="bg-black hover:bg-stone-400 px-[20px] py-[8px] rounded-md text-white" onClick={onCancel}>Cancel </button>
+          <button
+            className="bg-black hover:bg-stone-400 px-[20px] py-[8px] rounded-md text-white"
+            onClick={onCancel}
+          >
+            Cancel
+          </button>
         </li>
         <li>
-          <button className="bg-black hover:bg-stone-400 px-[20px] py-[8px] rounded-md text-white" onClick={handleSave} >Save</button>
+          <button
+            className="bg-black hover:bg-stone-400 px-[20px] py-[8px] rounded-md text-white"
+            onClick={handleSave}
+          >
+            Save
+          </button>
         </li>
       </menu>
-      <Input type="text" ref={title} label="title" />
+      <Input type="text" ref={title} label="Title" />
       <Input label="Description" ref={description} textarea />
       <Input type="date" ref={dueDate} label="Due Date" />
     </div>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,14 +1,21 @@
 
 
 // eslint-disable-next-line react/prop-types
-const SideBar = ({onSelected}) => {
+const SideBar = ({onSelected,title}) => {
+  console.log("title:",title);
+  
   return (
     <aside className="bg-black w-1/3 py-16 px-8 text-white md:w-72 rounded-r-xl h-screen">
     <h2 className="mb-8 text-2xl font-bold uppercase ">Your Project</h2>
     <div>
         <button className="px-4 py-2 text-xs md:text-xl bg-stone-700 text-stone-400 rounded-md hover:text-stone-100 hover:bg-stone-500" onClick={onSelected}> + Add Project</button>
     </div>
-    <ul></ul>
+    <ul>
+      {title.length > 0 && title.map((data)=>{
+         let classCss = `px-2 py-1  text-md text-left  hover:bg-stone-600 w-full rounded-md my-3 bg-stone-800  text-stone-200`;
+      return  <li key={data.id} className={classCss}>{data.title}</li>
+      })}
+    </ul>
 </aside>
 
   )
