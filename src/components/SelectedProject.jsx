3dbc853c3308533a675f76958417acda93a39f1a
premkumar199131 <premkumar.ashokan@talentship.io>
2025-02-15 09:52:38 +0530

    Project delete scenario

---
+++ src/components/SelectedProject.jsx
@@ -0,0 +1,27 @@
+
+const SelectedProject = ({project,onDelete}) => {
+    console.log("project>>>",project);
+    
+  return (
+    <div className="w-[500px] mt-6 py-4 leading-10">
+    <div className="flex justify-between items-center ">
+      <header className="text-2xl font-bold text-amber-950">
+        {project?.title}
+      </header>
+      <button
+        className="px-1  bg-stone-500 text-white font-bold rounded-md hover:bg-stone-300 hover:text-stone-600"
+      onClick={onDelete}
+      >
+        Delete
+      </button>
+    </div>
+    <p className="text-stone-500 ">{project?.dueDate}</p>
+    <p>{project?.description}</p>
+    <hr />
+
+   
+  </div>
+  )
+}
+
+export default SelectedProject
